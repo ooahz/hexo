56289cf87363f9b5f16df8160356446153e13b80
十玖八柒 <57132258+ooahz@users.noreply.github.com>
2021-10-22 15:56:38 +0800

    Update title.js

--- js/title.js
+++ js/title.js
@@ -3,11 +3,11 @@
  document.addEventListener('visibilitychange', function() {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/favicon.ico");
-         document.title = 'ヽ(●-`Д´-)ノ真的要离开了吗？';
+         document.title = '!!!∑(ﾟДﾟノ)ノ';
          clearTimeout(titleTime);
      } else {
          $('[rel="icon"]').attr('href', "/img/favicon.ico");
-         document.title = 'ヾ(Ő∀Ő3)ノ欢迎回家！' + OriginTitle;
+         document.title = '(〃'▽'〃)' + OriginTitle;
          titleTime = setTimeout(function() {
              document.title = OriginTitle;
          }, 2000);
